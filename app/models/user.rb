21ed8b1aa956776faf7fbbb15417edfbfa1582af
anhthai <nguyen.doan.anh.thai@sun-asterisk.com>
2023-08-31 16:44:37 +0700

    Feature: Sign up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
+  before_save :downcase_email!
+  validates :phone, presence: true
+  validates :name, presence: true, length: {maximum: Settings.users.max_name}
+  validates :password, presence: true,
+                       length: {minimum: Settings.users.min_password},
+                       format: {with: Settings.users.valid_password_regex}
+  validates :email, presence: true,
+                    length: {maximum: Settings.users.max_email},
+                    format: {with: Settings.users.valid_email_regex},
+                    uniqueness: true
+  has_secure_password
   # association
   has_many :favorite_pitches, dependent: :destroy
   has_many :reviews, dependent: :destroy
   has_many :bookings, dependent: :destroy
+  private
+  def downcase_email!
+    email.downcase!
+  end
 end
